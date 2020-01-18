8f08094c8696590917aec644278d5cd6a695fdbb
shaikh-imran1 <imranshaikh5937@gmail.com>
2020-01-18 20:42:13 +0530

    eureka-naming-server is created.

--- currency-conversion-service/src/main/java/com/imran/microservices/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/imran/microservices/CurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.imran.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.imran.microservices")
+@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication
 {
 	public static void main(String[] args)
